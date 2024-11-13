b4510961cbadaf200d94f3208984a532f657fa18
강민기 <codemingi1@gmail.com>
2024-11-13 15:53:16 +0900

    token authentification

--- data/auth.js
+++ data/auth.js
@@ -2,6 +2,7 @@ let users = [
     {
         id: '1',
         username: 'apple',
+        password: '1111',
         name: '김사과',
         email: 'apple@apple.com',
         url: 'https://img.freepik.com/premium-photo/young-asian-man-her-clean-face-with-fresh-healthy-skin-ai-generated_145713-6656.jpg'
@@ -9,6 +10,7 @@ let users = [
     {
         id: '2',
         username: 'banana',
+        password: '2222',
         name: '반하나',
         email: 'banana@banana.com',
         url: 'https://img.freepik.com/premium-photo/young-asian-man-her-clean-face-with-fresh-healthy-skin-ai-generated_145713-6656.jpg'
@@ -16,6 +18,7 @@ let users = [
     {
         id: '3',
         username: 'orange',
+        password: '3333',
         name: '오렌지',
         email: 'orange@orange.com',
         url: 'https://img.freepik.com/premium-photo/young-asian-man-her-clean-face-with-fresh-healthy-skin-ai-generated_145713-6656.jpg'
@@ -24,10 +27,10 @@ let users = [
 export async function createUser(username, password, name, email) {
     const user = {
         id: '4',
-        username: username,
-        password: password,
-        name: name,
-        email: email,
+        username,
+        password,
+        name,
+        email,
         url: 'https://img.freepik.com/premium-photo/young-asian-man-her-clean-face-with-fresh-healthy-skin-ai-generated_145713-6656.jpg'
     }
     users = [user, ...users]
@@ -37,4 +40,8 @@ export async function createUser(username, password, name, email) {
 export async function findByUsername(username) {
     const user = users.find((user) => user.username === username)
     return user
+}
+
+export async function findById(id) {
+    return users.find((user) => user.id === id)
 }
