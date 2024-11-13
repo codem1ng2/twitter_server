1e624d957e4b046cad419ba6c1f5e3005dec5925
강민기 <codemingi1@gmail.com>
2024-11-13 12:18:23 +0900

    암호화 추가적용

--- router/auth.js
+++ router/auth.js
@@ -1,14 +1,20 @@
 import express from 'express'
 import * as authController from '../controller/auth.js'
+import { body } from 'express-validator'
+import { validate } from '../middleware/validator.js'
 
 const router = express.Router()
 
-router.post('/signup', authController.signup)
+const validateTweet = [
+    body('username').trim().isLength({ min: 3 }).withMessage('최소 3자이상 입력').matches(/^[a-zA-Z0-9]*$/).withMessage('특수문자 사용불가'),
+    body('password').trim().isLength({ min: 4 }).withMessage('최소 4자이상 입력'),
+    body('email').trim().isEmail().withMessage('이메일 형식 확인'), validate
+]
+
+
+router.post('/signup', validateTweet, authController.signup)
 router.post('/login', authController.login)
 
-
-// 로그인 유지
-
-
+router.post('/me', authController.verify)
 
 export default router
